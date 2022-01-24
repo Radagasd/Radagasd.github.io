256c27aeb655c70bf48848ffb632d8c58f410f43
Kelvin <kelvinvh95@hotmail.com>
2022-01-24 14:35:44 +0100

    Added a dark theme, added shadows to the background, changed from $palette to :root system for color palette.

---
+++ assets/js/themeSelect.js
@@ -0,0 +1,27 @@
+const setTheme = (theme) => {
+    document.documentElement.className = theme;
+    localStorage.setItem('theme', theme);
+}
+
+const getTheme = () => {
+    const theme = localStorage.getItem('theme');
+    theme && setTheme(theme);
+}
+
+getTheme();
+
+const mySwitchTheme = _ =>
+{
+  if(document.documentElement.className == "light")
+  {
+    setTheme('dark');
+  }
+  else
+  {
+    setTheme('light');
+  }
+}
+
+document.getElementById('theme-select').addEventListener('change', function() {
+  setTheme(this.value);
+});
